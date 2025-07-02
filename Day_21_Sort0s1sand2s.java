05c49e00633c2f4ba6e37102ead292029dad1bad
LIPUN ROUT <lipunrout001@gmail.com>
2025-07-02 11:16:35 +0530

    Create Day_21_Sort0s1sand2s.java

---
+++ Day_21_Sort0s1sand2s.java
@@ -0,0 +1,29 @@
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        int zero=0;
+        int one=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                zero++;
+            }
+            else if(arr[i]==1){
+                one++;
+                
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(zero>0){
+                arr[i]=0;
+                zero--;
+            }
+            else if(one>0){
+                arr[i]=1;
+                one--;
+            }
+            else{
+                arr[i]=2;
+            }
+        }
+    }
+}
