18e0764bdc55f6b211dbfb6c54ade607a2f9a131
LIPUN ROUT <lipunrout001@gmail.com>
2025-07-25 16:18:57 +0530

    Create Countpairswithgivensum.java

---
+++ Countpairswithgivensum.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    int countPairs(int arr[], int target) {
+       int ans=0;
+       HashMap<Integer,Integer> mm =new HashMap<>();
+       for(int x:arr){
+           if(mm.get(target-x)!=null)ans+=mm.get(target-x);
+           mm.put(x,mm.getOrDefault(x,0)+1);
+       }
+        return ans;
+    }
+}
