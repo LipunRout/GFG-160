bd5eca269f1a31e5fcbb73ac0d0f3e467acf0ccd
LIPUN ROUT <lipunrout001@gmail.com>
2025-08-30 08:07:56 +0530

    Create Levelordertraversal.java

---
+++ Levelordertraversal.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        if(root==null) return ans;
+        
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size=q.size();
+            ArrayList<Integer> level=new ArrayList<>();
+            
+            for(int i=0;i<size;i++){
+                Node curr=q.poll();
+                level.add(curr.data);
+                
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right !=null) q.add(curr.right);
+                
+                
+            }
+            ans.add(level);
+            
+        }
+        return ans;
+    }
+}
